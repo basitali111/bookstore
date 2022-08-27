290dedaf0f42e81ad2373a416e30f26104062b6a
Basit Ali <allahbadshah148@gmail.com>
2022-08-28 02:15:47 +0500

    adding fils

---
+++ src/components/AddBook.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddBook = () => (
+  <div className="add-book-section">
+    <h3 className="add-book-section-title">Add a new book</h3>
+    <form className="add-book-form">
+      <input placeholder="Book Title" />
+      <input placeholder="Author" />
+      <button type="button">category</button>
+      <button type="button">Add book</button>
+    </form>
+  </div>
+);
+
+export default AddBook;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <div className="footer">
+    <p>Basit Ali 2022 MIT copyright</p>
+  </div>
+);
+
+export default Footer;
